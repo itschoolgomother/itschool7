8263a507fe22bf7aeba60b6f4aa8df170724cc66
itschoolgomother <itschoolgomother@gmail.com>
2020-01-30 19:28:39 +0200

    30.1.2020

--- Богдана/main.js
+++ Богдана/main.js
@@ -1,3 +1,24 @@
-var x =prompt("vashe chislo?","10")
-var y =prompt("vashe chislo?","77")
-alert(x*y)
+var say = prompt("Ваш пароль","password")
+var time = new Date();
+var mypas = ""+time.getMinutes()
+
+
+if (say == mypas) {
+alert("привет,Богдана")
+code()
+}else{
+	alert("пока")
+
+}
+
+
+
+function code(){
+var usd = 24.5
+var x = prompt ("ваше число?",10)
+
+var resutGrn = usd*x
+var resutUsd = x/usd
+alert(resutGrn.toFixed (2)+"грн")
+alert(resutUsd.toFixed (2)+"$")
+}
