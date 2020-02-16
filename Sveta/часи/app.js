d1b1777331ad021be8dd2403a6dacf38e9777f2c
itschoolgomother <itschoolgomother@gmail.com>
2020-02-16 19:28:39 +0200

    16.2.2020

--- Sveta/часи/app.js
+++ Sveta/часи/app.js
@@ -1,8 +1,19 @@
-setInterval(function() {
-           alert("Привет")
-}, 10000);
-
 var line = document.getElementById("line-m");
-var time = new Date();
-var min = 	time.getMinutes()*6;
-line.style.transform = "rotate("+min+"deg)";
+var lineh = document.getElementById("line-h");
+var timer = document.getElementById("time")
+
+setInterval(function() {
+          
+
+   var time = new Date();
+   var min = 	time.getMinutes()*6;
+   var hours = time.getHours ()*30;
+   line.style.transform =  "rotate("+min+ "deg)";
+   lineh.style.transform =  "rotate("+hours+ "deg)";
+   timer.innerHTML	 = time.getHours()+":"+time.getMinutes();
+},100);
+
+
+
+
+
