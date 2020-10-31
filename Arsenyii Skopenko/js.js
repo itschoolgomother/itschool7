642497c427ca3d17d4868feb4e2f74c93868499f
itschoolgomother <itschoolgomother@gmail.com>
2020-10-31 16:41:28 +0200

    31.10.2020

---
+++ Arsenyii Skopenko/js.js
@@ -0,0 +1,44 @@
+/*function meetUp(N, C) {
+	return Math.floor((12-C)/N);
+}
+let num = 5;
+console.log('num')
+	
+function getExchange(vidValuti, kurs, summa){
+	let kolvo = kurs*summa;
+	return `Mi vam dadim ${kolvo} ${vidValuti
+	}`;
+}
+
+function litres(time){
+	return Math.floor(time*0.5);
+}
+let a = true;
+let b = false;
+function posravnenie(){
+if (a == !b){
+	return true;
+	}
+	else{
+		return false;
+	}
+}
+let cashInWallet = 30; 
+let amount = 50;
+const price = 40;
+let enoughWallet = cashInWallet >= price;
+let enoughCard = amount>=price;
+let canBuy = enoughWallet && enoughCard;
+console.log(canBuy);
+
+let isWaterhot = true;
+let makeTea = true;
+let canMakeTea = isWaterhot && makeTea;
+*/
+let str = "Hello World"
+for (let i = 0 ; i <= str.length ; i++){
+	console.log(str[i])
+}
+function ghj(n) {
+	// body...
+}
